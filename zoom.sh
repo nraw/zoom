7d32a6ae0e7033bc56ee098e64663d567fc70b1a
Andrej-Marsic <andrej_marsic@mckinsey.com>
2023-03-23 15:43:58 +0100

    feat: replace separator

--- zoom.sh
+++ zoom.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh -e
-DATA=$(icalBuddy -n -b '' -nnr -ea -nc -eed -nrd -npn -ps "/ /" -po title,datetime,location eventsNow)
+DATA=$(icalBuddy -n -b '' -nnr -ea -nc -eed -nrd -npn -ps "/|/" -po title,datetime,location eventsNow)
 if [[ -z $DATA ]]; then
-	DATA=$(icalBuddy -n -li 3 -b '' -nnr -ea -nc -eed -nrd -npn -ps "/ /" -po title,datetime,location eventsToday)
+	DATA=$(icalBuddy -n -li 3 -b '' -nnr -ea -nc -eed -nrd -npn -ps "/|/" -po title,datetime,location eventsToday)
 fi
 SELECTION=$(echo "$DATA" | fzf -1 )
 echo "$SELECTION"
-URL=$(echo $SELECTION  | awk '{print $3}' )
+URL=$(echo $SELECTION  | awk -F'|' '{print $3}' )
 # open "http://www.google.com"
 open "$URL"
