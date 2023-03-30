b041bfdd042dff3542ddeb5faabdd5c2bf337f3f
Andrej-Marsic <andrej_marsic@mckinsey.com>
2023-03-30 15:11:35 +0200

    only include title datetime and location

--- zoom.sh
+++ zoom.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh -e
-DATA=$(icalBuddy -n -b '' -nnr -ea -nc -eed -nrd -npn -ps "/|/" -po title,datetime,location eventsNow)
+DATA=$(icalBuddy -n -b '' -nnr -ea -nc -eed -nrd -npn -ps "/|/" -po title,datetime,location -iep title,datetime,location eventsNow)
 if [[ -z $DATA ]]; then
-	DATA=$(icalBuddy -n -li 3 -b '' -nnr -ea -nc -eed -nrd -npn -ps "/|/" -po title,datetime,location eventsToday)
+	DATA=$(icalBuddy -n -li 3 -b '' -nnr -ea -nc -eed -nrd -npn -ps "/|/" -po title,datetime,location -iep title,datetime,location eventsToday)
 fi
 SELECTION=$(echo "$DATA" | fzf -1 )
 echo "$SELECTION"
